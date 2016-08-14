30610014c7ef63e33b462660f2adcab0e261f4de
nanno <minamino@minamino-no-MacBook-Pro.local>
2016-08-14 21:30:44 +0900

    20160814版

---
+++ Cask
@@ -0,0 +1,32 @@
+(source gnu)
+(source melpa)
+
+;;package
+(depends-on "auto-complete")
+(depends-on "auto-complete-rst")
+(depends-on "auto-install")
+(depends-on "color-moccur")
+(depends-on "dash-at-point")
+;;(depends-on "helm-mode")
+(depends-on "helm")
+(depends-on "helm-ag")
+(depends-on "helm-ack")
+(depends-on "helm-c-moccur")
+(depends-on "helm-c-yasnippet")
+(depends-on "helm-gtags")
+(depends-on "helm-etags-plus" :git
+	    "https://github.com/jixiuf/helm-etags-plus.git")
+
+(depends-on "markdown-mode")
+(depends-on "php-mode")
+(depends-on "php-completion"
+	    :git "https://github.com/suzuki/php-completion.git"
+	    :branch "develop"
+	    :files("*.el" "phpcmp-index-of-*"))
+(depends-on "yasnippet")
+(depends-on "web-mode")
+(depends-on "matlab-mode")
+(depends-on "python-mode")
+(depends-on "jedi")
+(depends-on "quickrun")
+(depends-on "swoop")
---
+++ init.el
@@ -0,0 +1,213 @@
+(set-language-environment "Japanese")
+(prefer-coding-system 'utf-8)
+(set-default-coding-systems 'utf-8)
+(set-terminal-coding-system 'utf-8)
+(set-keyboard-coding-system 'utf-8)
+(set-buffer-file-coding-system 'utf-8)
+
+;;default.el
+(setq inhibit-default-init t)
+
+;;起動時のメッセージを非表示
+(setq inhibit-startup-screen t)
+
+;;バックアップ作成させない
+(setq backup-inhibited t)
+
+;;終了時自動保存ファイルを削除
+(setq delete-auto-save-files t)
+
+;;末尾の余計な改行を消す
+(setq next-line-add-newlines nil)
+
+;;バックスラッシュ
+(global-set-key [?¥] [?\\])
+
+;;時間を表示
+(display-time)
+
+;;行番号
+(line-number-mode 1)
+(column-number-mode 1)
+;;(global-linum-mode t)
+
+;;対応する括弧の強調
+(setq show-paren-delay 0)
+(show-paren-mode t)
+
+;;選択範囲の可視化
+(setq-default transient-mark-mode t)
+
+;;折り返し表示
+(setq truncate-lines t)
+
+;;Cask
+(when (require 'cask  nil t)
+  (cask-initialize))
+
+;;GDB/GUD
+(defvar gud-gdb-history (list "gdb --annnotate=3 "))
+(setq gdb-many-windows t)
+(setq gdb-use-sparate-io-buffer t)
+
+(add-hook 'gdb-mode-hook
+	  '(lambda ()
+	     (gud-tooltip-mode t) ;;mauseover時に値を表示
+	     (gud-break-main nil)
+	     (gud-run nil)))
+
+;;helm
+(when (require 'helm-config nil t)
+  (helm-mode 1)
+  (global-set-key (kbd "C-x b") 'helm-mini)
+  (global-set-key (kbd "C-M-z") 'helm-resume)
+  ;;自動補完を無効にしTABで手動補完
+  (custom-set-variables '(helm-ff-auto-update-initial-value nil))
+  (define-key helm-read-file-map (kbd "TAB") 'helm-execute-persistent-action)
+  (define-key helm-read-file-map (kbd "C-h") 'delete-backward-char))
+(when (require 'helm-ack nil t)
+  (setq helm-c-ack-version 2)
+  (setq helm-c-ack-auto-set-filetype nil)
+  (setq helm-c-ack-thing-at-point 'symbol)
+  (global-set-key (kbd "C-x g") 'helm-ack))
+
+(when (require 'helm-c-moccur nil t)
+  (setq helm-c-moccur-helm-idle-delay 0.1)
+  (setq helm-c-moccur-higligt-info-line-flag t)
+  (setq helm-c-moccur-enable-auto-look-flag t)
+  (setq helm-c-moccur-enable-initial-pattern t)
+
+  ;;バッファ内検索
+  (define-key global-map (kbd "M-o") 'helm-c-moccur-occur-by-moccur)
+  ;;ディレクトリ
+  (define-key global-map (kbd "M-d") 'helm-c-moccur-dmoccur)
+  ;;現在選択中の候補の位置を他のwindowに表示する
+  (define-key global-map (kbd "s-o") 'helm-c-moccur-buffer-list)
+  (define-key isearch-mode-map (kbd "M-o") 'helm-c-moccur-from-isearch)
+)
+
+(when (require 'helm-gtags nil t)
+  (add-hook 'helm-gtags-mode-hook
+	    '(lambda ()
+	       (setq helm-gtags-ignore-case t)
+	       (local-set-key (kbd "M-t") 'helm-gtags-find-tag)
+	       (local-set-key (kbd "M-r") 'helm-gtags-find-rtag)
+	       (local-set-key (kbd "M-s") 'helm-gtags-find-symbol)
+	       (local-set-key (kbd "C-q") 'helm-gtags-pop-stack)
+	       (local-set-key (kbd "C-c C-f") 'helm-gtags-find-file))))
+
+(when (require 'helm-etags+ nil t)
+  (global-set-key (kbd "M-.") 'helm-etags+-select)
+  (global-set-key (kbd "M-*") 'helm-etags+-history-go-back))
+
+;;yasnippet
+(when (require 'yasnippet nil t)
+  (setq yas-snippet-dirs
+       '("~/.emacs.d/local/yasnippet/snippets"
+	 "~/.emacs.d/elisp/yasnippet/snippets"
+	 ))
+  (yas-global-mode 1))
+
+;;auto-complete
+(when (require 'auto-complete-config nil t)
+  (add-to-list 'ac-modes 'markdown-mode)
+  (add-to-list 'ac-modes 'js2-mode)
+  (add-to-list 'ac-modes 'web-mode)
+  (add-to-list 'ac-modes 'enh-ruby-mode)
+  (ac-config-default)
+  (setq ac-auto-start 4)
+  (setq ac-dwim t)
+  (setq ac-use-menu-map t)
+  (setq ac-comphist-file "~/.emacs.d/local/ac-comphist.dat")
+  (add-to-list 'ac-dictionary-directories "~/.emacs.d/ac-dict"))
+
+;markdown-mode
+(when (require 'markdown-mode nil t)
+  (add-to-list 'auto-mode-alist '("\\.markdown$" . markdown-mode))
+  (add-to-list 'auto-mode-alist '("\\.md$" . markdown-mode)))
+
+;;matlab-mode
+(when (require 'matlab-mode nil t)
+  (add-to-list 'auto-mode-alist '("\\.m$" . matlab-mode)))
+
+;web-mode
+(when (require 'web-mode nil t)
+  (setq php-mode-force-pear t)
+  (add-to-list 'auto-mode-alist '("\\.html$" . web-mode)))
+
+(when (require 'web-mode nil t)
+  (setq php-mode-force-pear t)
+  (add-to-list 'auto-mode-alist '("\\.htm$" . web-mode)))
+
+(when (require 'web-mode nil t)
+  (setq php-mode-force-pear t)
+  (add-to-list 'auto-mode-alist '("\\.php$" . web-mode)))
+
+(defun web-mode-hook ()
+  "Hooks for Web mode."
+  (setq web-mode-markup-indent-offset   2)
+  (setq web-mode-css-offset    2)
+  (setq web-mode-script-offset 2)
+  (setq web-mode-php-offset    2)
+  (setq web-mode-java-offset   2)
+  (setq web-mode-asp-offset    2))
+
+(add-hook 'web-mode-hook 'web-mode-hook)
+
+(add-hook 'php-mode-hook
+	  (lambda ()
+	    (when (require 'php-completion)
+	      (php-completion-mode t)
+	      (define-key php-mode-map (kbd "C-o") 'phpcmp-comlete)
+	      (make-local-variable 'ac-sources)
+	      (setq ac-sources '(
+				 ac-source-words-in-same-mode-buffers
+				 ac-source-php-completion
+				 ac-source-filename
+				 )))))
+
+;;yatex
+(add-to-list 'load-path "~/.emacs.d/site-lisp/yatex")
+;; YaTeX mode
+(setq auto-mode-alist
+      (cons (cons "\\.tex$" 'yatex-mode) auto-mode-alist))
+(autoload 'yatex-mode "yatex" "Yet Another LaTeX mode" t)
+(setq tex-command "platex")
+(setq dviprint-command-format "dvipdfmx %s")
+;; use Preview.app
+(setq dvi2-command "open -a Preview")
+(setq bibtex-command "pbibtex")
+		  
+;;python-mode
+(when (require 'python-mode nil t)
+  (add-to-list 'auto-mode-alist '("\\.py$" . python-mode)))
+
+;;jedi
+(add-hook 'python-mode-hook
+	  'jedi:setup
+	  '(lambda ()
+	     (setq indent-tabs-mode nil)
+	     (setq indent-level 4)
+	     (setq python-indent 4)
+	     (setq tab-width 4)))
+(setq jedi:complete-on-dot t)
+
+;;docstring comment
+;; C-c dで関数のコメントを作成
+(defun python-docstring-comment()
+  (interactive)
+  (let* ((begin-point (point-at-bol))
+	 (end-point (point-at-eol))
+	 (function-line (buffer-substring begin-point end-point))
+	 (space (format "    %s" (replace-regexp-in-string "def.*" "" function-line))))
+    (goto-char end-point)
+    (insert "\n")
+    (insert (format "%s\"\"\"\n" space))
+    (when (string-match ".*(\\(.+\\)):" function-line)
+      (dolist (arg (split-string (match-string 1 function-line) ","))
+	(if (not (equal arg "self"))
+	    (insert (format "%s:param TYPE %s:\n" space (replace-regexp-in-string "^\\s-+\\|\\s-+$" "" arg))))))
+    (insert (format "%s:rtype: TYPE\n" space))
+    (insert (format "%s\"\"\"" space))))
+
+(define-key python-mode-map (kbd "C-c d") 'python-docstring-comment)
